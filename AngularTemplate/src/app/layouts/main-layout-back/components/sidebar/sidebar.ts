77b5c3d26f8f77e67b7297a20802263cf52b910f
Juan <juan@email.com>
2026-03-18 18:00:21 +0100

    Ja

--- AngularTemplate/src/app/layouts/main-layout-back/components/sidebar/sidebar.ts
+++ AngularTemplate/src/app/layouts/main-layout-back/components/sidebar/sidebar.ts
@@ -1,9 +1,13 @@
 import {Component, inject, signal} from '@angular/core';
 import {LayoutService} from "../../layautService";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
